89ebbf092155b5a6a141ece0295d7b31ec31c9be
Giles Anthony Villamor II <villamorbudz@gmail.com>
2024-05-01 00:53:41 +0800

    updated splash,login, and sign up UI

--- app/src/main/java/com/example/quol/signUp.java
+++ app/src/main/java/com/example/quol/signUp.java
@@ -15,8 +15,9 @@ public class signUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Button birthdateBtn = findViewById(R.id.userAuth_birthdateBtn);
-        birthdateBtn.setOnClickListener(view -> openDatePicker());
+//        Button birthdateBtn = findViewById(R.id.userAuth_birthdateBtn);
+//        birthdateBtn.setOnClickListener(view -> openDatePicker());
+
         TextView loginRedirect = findViewById(R.id.loginRedirect);
         loginRedirect.setOnClickListener(view -> {
             Intent redirect = new Intent(signUp.this, login.class);
@@ -25,21 +26,21 @@ public class signUp extends AppCompatActivity {
         });
     }
 
-    private void openDatePicker() {
-        TextView birthdateText = findViewById(R.id.signUp_birthdateText);
-
-        final Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        DatePickerDialog dialog = new DatePickerDialog(this,
-                (datePicker, selectedYear, selectedMonth, selectedDay) ->
-                        birthdateText.setText((selectedMonth+1) + "/" + selectedDay + "/" + selectedYear),
-                year, month, day);
-
-        dialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-
-        dialog.show();
-    }
+//    private void openDatePicker() {
+//        TextView birthdateText = findViewById(R.id.signUp_birthdateText);
+//
+//        final Calendar calendar = Calendar.getInstance();
+//        int year = calendar.get(Calendar.YEAR);
+//        int month = calendar.get(Calendar.MONTH);
+//        int day = calendar.get(Calendar.DAY_OF_MONTH);
+//
+//        DatePickerDialog dialog = new DatePickerDialog(this,
+//                (datePicker, selectedYear, selectedMonth, selectedDay) ->
+//                        birthdateText.setText((selectedMonth+1) + "/" + selectedDay + "/" + selectedYear),
+//                year, month, day);
+//
+//        dialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+//
+//        dialog.show();
+//    }
 }
